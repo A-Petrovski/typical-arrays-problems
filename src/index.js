07cd34ba8f884ee9bc1d225cb4f62c5a37de98e4
Alexander Petrovski <APetrovski5069296@gmail.com>
2020-09-30 11:50:42 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+    if (!!!array || array.length > 1) {
+        return array.sort((a, b) => a - b ).shift();
+    } else
+    {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max (array = []) {
+    if (!!!array || array.length > 1) {
+        return array.sort((a, b) => a - b ).pop();
+    } else
+    {
+        return 0;
+    }  
+  }
 
 exports.avg = function avg (array) {
+if (!!array) 
+  if (array.length > 1) {
+    return array.reduce((sum, current) => sum + current, 0)/array.length; 
+  } else
+  {
+    return 0;
+  }
   return 0;
 }
